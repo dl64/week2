72eb5902c5fb34e33b76a170f59370b38ec6e19b
Dmitry Lepikhov <dmitry@lepikhov.ru>
2020-05-31 15:39:10 +0300

    Домашнее задание Exception

---
+++ homework_exception_1.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        user_answer1 = input('Как дела? ')
+        if user_answer1 == 'Хорошо':
+            print('Молодец! Вопросов больше нет!')
+            print('*****')
+            break
+        else:
+            print('Разве "{}" это подходящий ответ?'.format(user_answer1))
+            print('Попробуй еще разок. Как дела?')
+            print('=====')
+    except KeyboardInterrupt:
+        print('\nэто сработал эксепшн')
+        print('Пока!')
+        break
